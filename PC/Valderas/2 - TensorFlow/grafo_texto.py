cc717adafc790264e3e114b4359882d2df38ee79
Valderas7 <45321489+Valderas7@users.noreply.github.com>
2020-06-11 17:36:30 +0200

    Update grafo_texto.py

--- PC/Valderas/2 - TensorFlow/grafo_texto.py
+++ PC/Valderas/2 - TensorFlow/grafo_texto.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
-# Lee el grafo:
+# Lee el modelo binario de TF:
 with tf.gfile.FastGFile('/home/user/T-F-M/Valderas/modelos/modelo.pb', 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
 
-# Elimina nodos Const.
+# Elimina nodos 'Const'.
 for i in reversed(range(len(graph_def.node))):
     if graph_def.node[i].op == 'Const':
         del graph_def.node[i]
@@ -15,6 +15,6 @@ for i in reversed(range(len(graph_def.node))):
         if attr in graph_def.node[i].attr:
             del graph_def.node[i].attr[attr]
 
-# Guarda como texto
+# Guarda como archivo de texto en el PC:
 tf.train.write_graph(graph_def, "modelo", "modelo.pbtxt", as_text=True)
 
